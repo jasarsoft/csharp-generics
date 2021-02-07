f3f7992594b3bc31b792af801502f8d83488c866
Edin Jašarević <edinjasar14@gmail.com>
2021-02-07 13:34:04 +0100

    copy and paste for victory

--- DataStructures/DataStructures/CircularBuffer.cs
+++ DataStructures/DataStructures/CircularBuffer.cs
@@ -51,4 +51,54 @@ namespace DataStructures
             get { return (_end + 1) % _buffer.Length == _start;  }
         }       
     }
+
+    public class CircularBufferString
+    {
+        private string[] _buffer;
+        private int _start;
+        private int _end;
+
+        public CircularBufferString() : this(capacity: 10)
+        {
+        }
+
+        public CircularBufferString(int capacity)
+        {
+            _buffer = new string[capacity + 1];
+            _start = 0;
+            _end = 0;
+        }
+
+        public void Write(string value)
+        {
+            _buffer[_end] = value;
+            _end = (_end + 1) % _buffer.Length;
+            if (_end == _start)
+            {
+                _start = (_start + 1) % _buffer.Length;
+            }
+        }
+
+        public string Read()
+        {
+            var result = _buffer[_start];
+            _start = (_start + 1) % _buffer.Length;
+            return result;
+        }
+
+        public int Capacity
+        {
+            get { return _buffer.Length; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return _end == _start; }
+        }
+
+        public bool IsFull
+        {
+            get { return (_end + 1) % _buffer.Length == _start; }
+        }
+    }
 }
